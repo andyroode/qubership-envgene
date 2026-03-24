d68d6370bb9e1a4f5c6ae1f71770e0ad220981ec
Siva Reddy Kunduru <35566000+sivareddyit@users.noreply.github.com>
2026-03-24 13:37:12 +0530

    fix: added authorization for downloading the zip file from artifactory using registry credentials (#1167)

--- python/artifact-searcher/artifact_searcher/artifact.py
+++ python/artifact-searcher/artifact_searcher/artifact.py
@@ -420,9 +420,10 @@ def check_artifact(repo_url: str, group_id: str, artifact_id: str, version: str,
     folder = version_to_folder_name(version)
     filename = create_artifact_name(artifact_id, artifact_extension, version, classifier)
     full_url = urljoin(base, f"{group_id}/{artifact_id}/{folder}/{filename}")
-
+    auth = HTTPBasicAuth(cred.username, cred.password) if cred else None
+    
     try:
-        response = requests.head(full_url, timeout=DEFAULT_REQUEST_TIMEOUT)
+        response = requests.head(full_url, auth=auth, timeout=DEFAULT_REQUEST_TIMEOUT)
         if response.status_code == 200:
             logger.info(
                 f"[Repository: {repo_url}] [Artifact: {group_id}:{artifact_id}:{version}] - Artifact found: {full_url}"

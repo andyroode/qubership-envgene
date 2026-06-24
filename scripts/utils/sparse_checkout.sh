c2c8baef8466807ef336a3914f71ea5ba3f021f5
Nurlybek Kamelov <79522742+GlimmerCape@users.noreply.github.com>
2026-06-24 14:00:32 +0500

    fix: sparse checkout issue @cf_skip (#1525)

--- scripts/utils/sparse_checkout.sh
+++ scripts/utils/sparse_checkout.sh
@@ -11,8 +11,7 @@ cd "${CI_PROJECT_DIR}"
 git config --global init.defaultBranch main
 git init .
 git remote add origin "${CI_REPOSITORY_URL}"
-git -c protocol.version=2 fetch --depth=1 origin \
-  "+refs/heads/${CI_COMMIT_REF_NAME}:refs/remotes/origin/${CI_COMMIT_REF_NAME}"
+git -c protocol.version=2 fetch --depth=1 origin "${CI_COMMIT_SHA}"
 git checkout --force "${CI_COMMIT_SHA}"
 git sparse-checkout init --cone
 git sparse-checkout set "$@"
